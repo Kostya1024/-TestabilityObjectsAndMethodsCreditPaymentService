240fa91ee9f9eb047c04bdfaada394fee34fc8bb
Kostya <snaaack@.mail.ru>
2022-06-13 17:39:15 +0200

    Adding logic to the CreditPaymentService method and calling the CreditPaymentService method in the body of the program for three different test data values

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,3 +1,8 @@
 public class CreditPaymentService {
-
+    public long calculate(int creditAmount, double interestRate, int creditTermYear) {
+        int creditTermMonth = creditTermYear * 12;
+        double annuityPaymentAccurate = creditAmount * ((interestRate / 100 / 12) * Math.pow((1 + (interestRate / 100 / 12)), creditTermMonth)) / (Math.pow((1 + (interestRate / 100 / 12)), creditTermMonth) - 1);
+        long annuityPayment = (long) annuityPaymentAccurate;
+        return annuityPayment;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
 
+        CreditPaymentService service = new CreditPaymentService();
+
+        long annuityPayment1 = service.calculate(1_000_000, 9.99, 1);
+        System.out.println("ОР 87991; ФР " + annuityPayment1);
+        long annuityPayment2 = service.calculate(1_000_000, 9.99, 2);
+        System.out.println("ОР 46140; ФР " + annuityPayment2);
+        long annuityPayment3 = service.calculate(1_000_000, 9.99, 3);
+        System.out.println("ОР 32262; ФР " + annuityPayment3);
+    }
 }
